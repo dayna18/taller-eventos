7d4191742bfec4df4b1288c44293da24b0edeb07
dayna18 <ailica.dec@gmail.com>
2024-08-15 01:31:19 -0300

    Agregamos boton

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+var div = document.querySelector('.contador');
+
+div.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
+
+});
