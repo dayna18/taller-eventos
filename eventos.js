3a14b8964b1c2e833988a511ea23466bfcde9898
dayna18 <ailica.dec@gmail.com>
2024-08-21 13:57:09 -0300

    Cambios

--- eventos.js
+++ eventos.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-var midiv= document.getElementById('midiv');
-
-midiv.addEventListener('click', function() {
+document.getElementById('midiv').addEventListener('click', function() {
     alert('Hola! Soy el div');
-});
-
 });
